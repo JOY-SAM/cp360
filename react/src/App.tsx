2ab135c04183c69c1180ad649e29ced6227d59fb
JOY <joysam1111@gmail.com>
2025-10-24 17:33:32 +0530

    Redux Implemented

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -2,34 +2,62 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import {format} from 'date-fns/format'
+import {parse} from 'date-fns/parse'
+import {startOfWeek} from 'date-fns/startOfWeek'
+import {getDay} from 'date-fns/getDay'
+import {enUS} from 'date-fns/locale/en-US'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const dummy_data = {
+  "01-09-2025": [
+      {"user_1": 1},
+      {"user_2": 2},
+      {"user_3": 3},
+      {"user_4": 4},
+  ],
+  "02-09-2025": [
+      {"user_1": 1},
+      {"user_2": 2},
+      {"user_3": 3},
+      {"user_4": 4},
+  ],
+  "03-09-2025": [
+      {"user_1": 1},
+      {"user_2": 2},
+      {"user_3": 3},
+      {"user_4": 4},
+  ],
+  "04-09-2025": [
+      {"user_1": 1},
+      {"user_2": 2},
+      {"user_3": 3},
+      {"user_4": 4},
+  ]
+}
+const locales = {
+  'en-US': enUS,
 }
 
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const App = (props:any) => (
+  <div>
+    <Calendar
+      localizer={localizer}
+      events={[]}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
+)
 export default App
